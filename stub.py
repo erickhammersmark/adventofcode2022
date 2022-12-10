c23ccb053e30f67e7afe08894ccbda4f55d8abca
Erick Hammersmark <bink@uffda.com>
2022-12-10 08:56:21 -0800

    add sample input support to the stub

--- stub.py
+++ stub.py
@@ -3,7 +3,13 @@ Advent of Code day X
 https://adventofcode.com/2022/day/X
 """
 
-with open("input", "r") as INPUT:
-    lines = (l.strip() for l in INPUT.readlines())
+import sys
+
+input_file = "input"
+if len(sys.argv) > 1 and sys.argv[1] == "sample":
+    input_file = "sample_input"
+
+with open(input_file, "r") as INPUT:
+    lines = [l.strip() for l in INPUT.readlines()]
 
 
